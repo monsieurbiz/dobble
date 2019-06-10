179cd2c382fd40d1b2500155f189d117c28dccf9
Maxime Huran <m.huran@monsieurbiz.com>
2019-06-10 14:52:58 +0200

    Generate index.html file in specified folder with rendered deck template

--- src/Generator/Html/Generator.php
+++ src/Generator/Html/Generator.php
@@ -6,9 +6,13 @@ namespace Mbiz\Dobble\Generator\Html;
 
 use Mbiz\Dobble\DeckInterface;
 use Mbiz\Dobble\Generator\GeneratorInterface;
+use Twig\Environment as TwigEnvironment;
+use Twig\Loader\FilesystemLoader as TwigFilesystemLoader;
 
 class Generator implements GeneratorInterface
 {
+    const TEMPLATE_FOLDER = 'templates';
+    const CACHER_FOLDER = 'cache';
 
     /** @var string */
     private $outputDirectory;
@@ -25,11 +29,24 @@ class Generator implements GeneratorInterface
     
     /**
      * @param DeckInterface $deck
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
     public function generate(DeckInterface $deck): void
     {
         if (!is_dir($this->outputDirectory)) {
             mkdir($this->outputDirectory);
         }
+    
+        $loader = new TwigFilesystemLoader(self::TEMPLATE_FOLDER);
+        $twig = new TwigEnvironment($loader, [
+            'cache' => self::CACHER_FOLDER,
+        ]);
+        
+        file_put_contents(
+            sprintf('%s/%s', $this->outputDirectory, 'index.html'),
+            $twig->render('deck.html.twig', ['deck' => $deck])
+        );
     }
 }
