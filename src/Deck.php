cb0905803c2d5ab9b35392aecea8c3467e77060b
Philippe Remdonck <p.raemdonck@monsieurbiz.com>
2019-06-10 14:18:23 +0200

    Deck creation.

--- src/Deck.php
+++ src/Deck.php
@@ -3,23 +3,51 @@ declare(strict_types=1);
 
 namespace Mbiz\Dobble;
 
-
+use Marmelab\Dobble\Deck as BaseDeck;
 use Marmelab\Dobble\DeckGenerator;
+use Marmelab\Dobble\DeckValidator;
+use Marmelab\Dobble\DobbleException;
 
-class Deck implements DeckInterface
+class Deck implements DeckInterface, \Countable
 {
     /**
      * @var array
      */
     const NUMBER_OF_ELEMENTS_PER_CARD = 8;
 
+    /**
+     * @var BaseDeck
+     */
+    private $deck;
+
+    /**
+     * Deck constructor.
+     */
     public function __construct()
     {
-        $deckGenerator = new DeckGenerator(self::NUMBER_OF_ELEMENTS_PER_CARD, DeckGenerator::ALGO_DEFAULT);
+        $deckGenerator = new DeckGenerator(self::NUMBER_OF_ELEMENTS_PER_CARD);
+        $this->deck = $deckGenerator->generate();
     }
 
     public function getCards(): array
     {
-        // TODO: Implement getCards() method.
+        return $this->deck->getCards();
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->deck);
+    }
+
+    /**
+     * @return bool
+     * @throws DobbleException
+     */
+    public function validate()
+    {
+        return DeckValidator::validate($this);
     }
 }
